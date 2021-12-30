1b94dc0a6b9ac0505bb9e2159322ab0156c738e3
sindhusenthil <sindhubsenthilkumar@gmail.com>
2021-12-29 22:01:03 -0800

    final project commit

---
+++ finalproject/led_states.h
@@ -0,0 +1,14 @@
+#ifndef __LED_STATES_H
+#define __LED_STATES_H
+
+enum BTN_STATE 
+{
+    LED_OFF,
+    LED_SOLID,
+    LED_BLINK_SLOW,
+    LED_BLINK_FAST,
+    LED_DIM_SLOW,   
+    LED_DIM_FAST    
+};
+
+#endif
